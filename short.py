9d7e90726202914d271ad37c764ed5d1bf7cff86
vishh123 <vishal23@navgurukul.org>
2024-05-24 15:09:07 +0530

    first comment

---
+++ short.py
@@ -0,0 +1,12 @@
+# a=90
+# print(type(a))
+
+# a=-3 + 4 * 2
+# print(a)
+
+# print( max(1,7,4,3,9,2,0,))
+# print( min(1,7,4,3,9,2,0,))
+
+# print(int(23.345))
+# print(float(1234.345))
+# print(int('234') * 2)
